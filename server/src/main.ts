70b5b5685008351149a1100580eb18651bb0a6f9
pashShmil <pavels@realcemmerce.co.il>
2023-07-10 16:10:42 +0300

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
+import { Controller, Module, Get } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+@Controller()
+class AppController {
+  @Get()
+  getRootRoute() {
+    return 'hi there!';
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   await app.listen(3000);
 }
 bootstrap();
