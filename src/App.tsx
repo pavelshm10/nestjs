b529bc7fbd0426ce1ed9b1c6c1cae77f588f9bbc
pashShmil <pavels@realcemmerce.co.il>
2024-04-08 17:47:58 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <Routes>
-        {/* <Route path="/about" element={<About/>}/> */}
+        <Route path="/*" element={<Home/>}/>
         <Route path="/users" element={MemoizedUsersComponent} />
       </Routes>
     </div>)
