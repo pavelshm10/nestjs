638df2a4b29d59a0b1f9d8b68d88409b109604b1
pavel <pavelsh00@gmail.com>
2024-07-16 09:45:11 +0300

    data

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -3,41 +3,41 @@ import { Product } from '../../types/product';
 import classes from '../products/ProductList.module.scss';
 
 const ProductList = () => {
-    // const [products, setProducts] = useState<Product[]>([]);
-    // const apiBaseUrl = process.env.REACT_APP_API_URL;
+    const [products, setProducts] = useState<Product[]>([]);
+    const apiBaseUrl = process.env.REACT_APP_API_URL;
 
-    // useEffect(() => {
-    //     console.log({apiBaseUrl});
-    //     if(!apiBaseUrl) return;
-    //     const fetchData = async () => {
-    //       try {
-    //         const response = await fetch(`${apiBaseUrl}/api/products`);
+    useEffect(() => {
+        console.log({apiBaseUrl});
+        if(!apiBaseUrl) return;
+        const fetchData = async () => {
+          try {
+            const response = await fetch(`${apiBaseUrl}/api/products`);
 
-    //         console.log({response})
-    //         if (!response.ok) {
-    //           throw new Error(`HTTP error! Status: ${response.status}`);
-    //         }
-    //         const data = await response.json();
-    //         setProducts(data);
-    //       } catch (error) {
-    //         console.error('Error fetching products:', error);
-    //       }
-    //     };
+            console.log({response})
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            setProducts(data);
+          } catch (error) {
+            console.error('Error fetching products:', error);
+          }
+        };
     
-    //     fetchData();
-    //   }, [apiBaseUrl]);
+        fetchData();
+      }, [apiBaseUrl]);
 
     return (
         <div>
             <h1>Product List</h1>
             <ul className={classes.container}>
-                {/* {products.map(product => (
+                {products.map(product => (
                     <li key={product.id}>
                         <h2>{product.name}</h2>
                         <p>Price: ${product.price}</p>
                         <p>{product.description}</p>
                     </li>
-                ))} */}
+                ))}
             </ul>
         </div>
     );
