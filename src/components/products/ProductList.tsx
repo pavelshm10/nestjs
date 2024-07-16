3a36342f3d100b87e73f863b800f9bc0017bffef
pavel <pavelsh00@gmail.com>
2024-07-16 10:32:16 +0300

    asd

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -1,51 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { Product } from '../../types/product';
-import classes from '../products/ProductList.module.scss';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Product } from "../../types/product";
+import classes from "../products/ProductList.module.scss";
+import axios from "axios";
 
 const ProductList = () => {
-    const [products, setProducts] = useState<Product[]>([]);
-    const apiBaseUrl = process.env.REACT_APP_API_URL;
+  const [products, setProducts] = useState<Product[]>([]);
+  const apiBaseUrl = process.env.REACT_APP_API_URL;
 
-    useEffect(() => {
-        console.log({apiBaseUrl});
-        if(!apiBaseUrl) return;
-        const fetchData = async () => {
-          try {
-            // const response = await fetch(`${apiBaseUrl}/api/products`);
-            const response = await axios.get(`${apiBaseUrl}/api/products`, {
-                headers: {
-                  'Content-Type': 'application/json'
-                }
-              });
-            console.log({response})
-            if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const data = await response.json();
-            setProducts(data);
-          } catch (error) {
-            console.error('Error fetching products:', error);
-          }
-        };
-    
-        fetchData();
-      }, [apiBaseUrl]);
+  useEffect(() => {
+    console.log({ apiBaseUrl });
+    if (!apiBaseUrl) return;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiBaseUrl}/api/products`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        //   credentials: "include",
+        });
+        console.log({ response });
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
 
-    return (
-        <div>
-            <h1>Product List</h1>
-            <ul className={classes.container}>
-                {products.map(product => (
-                    <li key={product.id}>
-                        <h2>{product.name}</h2>
-                        <p>Price: ${product.price}</p>
-                        <p>{product.description}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    fetchData();
+  }, [apiBaseUrl]);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul className={classes.container}>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>Price: ${product.price}</p>
+            <p>{product.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ProductList;
